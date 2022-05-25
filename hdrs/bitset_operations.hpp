f93a17b644ca0a9a0e637b92c2c157f6f0308744
LinearBasis <memch@LAPTOP-E358FJPV.localdomain>
2022-05-26 00:24:32 +0300

    subset works

--- hdrs/bitset_operations.hpp
+++ hdrs/bitset_operations.hpp
@@ -2,15 +2,23 @@
 #include <bitset>
 #include <cstdlib>
 
+
+
+
+// cdef123456
+// 9876543210
+
+// 123456
+// 543210
+
 template <size_t n, size_t size>
 std::bitset<size> subset(const std::bitset<n> &number, size_t min, size_t max)
 {
-    // return std::bitset<max - min>(number.to_string().substr(min, max - min));
     std::bitset<size>  ans;
 
     for (size_t i = min; i < max; ++i)
     {
-        ans[i - min] = number[i];
+        ans[size - 1 - (i - min)] = number[n - 1 - i];
     }
     return (ans);
 }
